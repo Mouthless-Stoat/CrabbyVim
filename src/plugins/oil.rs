9b01b8a4f5ab388376993fcff1a29b800bdccf30
Mouthless Stoat <nphuyanh@outlook.com>
2025-08-12 15:56:08 -0300

    refactor: factor out opts macro so the formatter work again

--- src/plugins/oil.rs
+++ src/plugins/oil.rs
@@ -6,48 +6,49 @@ use crate::{lua_table, require};
 use super::Plugins;
 
 pub fn plugins() -> Plugins {
+    let opts = lua_table! {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+
+        keymaps = {
+            ["q"] = { "actions.close" },
+        },
+        view_options = {
+            show_hidden = true,
+            highlight_filename = function(entry)
+                local _, hl = require("nvim-web-devicons").get_icon(entry.name, vim.fn.fnamemodify(entry.name, ":e"))
+                return hl
+            end
+        },
+        float = {
+            padding = 10,
+            border = "single",
+            preview_split = "right",
+            override = function(conf)
+                conf.style = "minimal"
+                return conf
+            end,
+        },
+        preview_win = {
+            border = "single"
+        },
+        confirmation = {
+            border = "single"
+        },
+        progress = {
+            border = "single"
+        }
+    };
+
     Ok(vec![
         LazyPlugin::new("stevearc/oil.nvim")
-            .opts(lua_table! {
-                default_file_explorer = true,
-                delete_to_trash = true,
-                skip_confirm_for_simple_edits = true,
-
-                keymaps = {
-                    ["q"] = { "actions.close" },
-                },
-                view_options = {
-                    show_hidden = true,
-                    highlight_filename = function(entry)
-                        local _, hl = require("nvim-web-devicons").get_icon(entry.name, vim.fn.fnamemodify(entry.name, ":e"))
-                        return hl
-                    end
-                },
-                float = {
-                    padding = 10,
-                    border = "single",
-                    preview_split = "right",
-                    override = function(conf)
-                        conf.style = "minimal"
-                        return conf
-                    end,
-                },
-                preview_win = {
-                    border = "single"
-                },
-                confirmation = {
-                    border = "single"
-                },
-                progress = {
-                    border = "single"
-                }
-            })
+            .opts(opts)
             .lazy_load(
-                LazyLoad::new(true)
-                    .add_key(LazyKey::new("<Leader>f").action(|| {
-                        require("oil")?.call_function::<()>("toggle_float", ())?;
-                        Ok(())
-                    })),
+                LazyLoad::new(false).add_key(LazyKey::new("<Leader>f").action(|| {
+                    require("oil")?.call_function::<()>("toggle_float", ())?;
+                    Ok(())
+                })),
             ),
     ])
 }
