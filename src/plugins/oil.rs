743576c832e4ae1b46ce5110070aaa69532696e7
Mouthless Stoat <nphuyanh@outlook.com>
2025-08-25 14:12:26 -0300

    feat(oil): replace oil key with just `-`

--- src/plugins/oil.rs
+++ src/plugins/oil.rs
@@ -23,44 +23,11 @@ pub(crate) fn plugins() -> Plugins {
                 return hl
             end
         },
-        float = {
-            padding = 0,
-            border = "single",
-            preview_split = "right",
-            override = function(conf)
-                local uis = vim.api.nvim_list_uis()[1]
-                local height = uis.height
-                local width = uis.width
-
-                conf.height = math.floor(height * 0.4)
-                conf.width = width
-                conf.row = height - conf.height
-
-                conf.border = { "", "─", "", "", "", "", "", "" }
-                conf.style = "minimal"
-
-                return conf
-            end,
-        },
-        preview_win = {
-            border = "single"
-        },
-        confirmation = {
-            border = "single"
-        },
-        progress = {
-            border = "single"
-        }
     };
 
     Ok(vec![
         LazyPlugin::new("stevearc/oil.nvim")
             .opts(opts)
-            .lazy_load(
-                LazyLoad::new(false).add_key(LazyKey::new("<Leader>f").action(|| {
-                    require("oil")?.call_function::<()>("toggle_float", ())?;
-                    Ok(())
-                })),
-            ),
+            .lazy_load(LazyLoad::new(false).add_key(LazyKey::new("-").action("<cmd>Oil<cr>"))),
     ])
 }
