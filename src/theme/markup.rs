6324367da7941a5c1e2bb57fac72f585991d8708
Mouthless Stoat <nphuyanh@outlook.com>
2025-08-31 11:43:33 -0300

    chore(theme): add more markup highlight

--- src/theme/markup.rs
+++ src/theme/markup.rs
@@ -5,6 +5,9 @@ pub(crate) fn highlights() -> nvim_oxi::Result<()> {
     set_markup_hl("link", HighlightOpt::with_fg(Blue).bold())?;
     set_markup_hl("quote", HighlightOpt::with_fg(Gray))?;
 
+    set_markup_hl("list.checked", HighlightOpt::with_fg(Green))?;
+    set_markup_hl("list.unchecked.", HighlightOpt::with_fg(Red))?;
+
     set_markup_hl("heading.1", HighlightOpt::with_fg(Blue))?;
     set_markup_hl("heading.2", HighlightOpt::with_fg(Green))?;
     set_markup_hl("heading.3", HighlightOpt::with_fg(Yellow))?;
