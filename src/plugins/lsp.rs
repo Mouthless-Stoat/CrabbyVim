efeac85bcac542cb0e4deb8a969e809b845e32ff
Mouthless Stoat <nphuyanh@outlook.com>
2025-09-05 20:54:20 -0300

    Revert "feat!: remove mason"
    This reverts commit f31f6b8d717e771caba6e3c4a6212038a514b5b0.

--- src/plugins/lsp.rs
+++ src/plugins/lsp.rs
@@ -1,3 +1,17 @@
+use crate::icons;
+use crate::lazy::LazyPlugin;
+use crate::table;
+
 plugin! {
     blink_cmp with highlights;
+    ---
+    LazyPlugin::new("mason-org/mason.nvim").opts(table! {
+        ui = table! {
+            icons = table!{
+                package_installed = icons::INSTALLED,
+                package_pending = icons::PENDING,
+                package_uninstalled = icons::UNINSTALLED
+            }
+        }
+    });
 }
